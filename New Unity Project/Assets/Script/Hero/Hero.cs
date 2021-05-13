bce0f4bdf1d5659ce34ac021df73358313567512
valek97 <kritenko97@gmail.com>
2021-05-13 23:42:01 +0300

    Изменения в диз доке

---
+++ New Unity Project/Assets/Script/Hero/Hero.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour
+{
+    [SerializeField] private float speed = 3f;
+    [SerializeField] private float lives = 5;
+    [SerializeField] private float jumpForce = 15f;
+    private bool isGround = false;
+
+    private Rigidbody2D rb;
+    private SpriteRenderer sprite;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponentInChildren<SpriteRenderer>();
+    }
+    private void Run()
+    {
+        Vector3 dir = transform.right * Input.GetAxis("Horizontal");
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + dir, speed * Time.deltaTime);
+        sprite.flipX = dir.x < 0.0f;
+    }
+
+    private void Jump()
+    {
+        rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+    }
+    private void CheckGround()
+    {
+        Collider2D[] collider = Physics2D.OverlapCircleAll(transform.position, 0.3f);
+        isGround = collider.Length > 1;
+    }
+    private void FixedUpdate()
+    {
+        CheckGround();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Horizontal"))
+            Run();
+        if (isGround && Input.GetButton("Jump"))
+            Jump();
+    }
+}
